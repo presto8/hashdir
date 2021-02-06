5d6b420ad99abc51025eb5895f0c43f0bc547bf3
Preston Hunt <me@prestonhunt.com>
2021-02-06 11:44:47 -0800

    add bash implementation

---
+++ bash/hashdir.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+    hash=$(get_hashes "$1" | sort | sha256sum - | cut -c1-8)
+    echo "$hash  $1"
+}
+
+get_hashes() {
+    cd "$1"
+    find . -type f -o -type l -printf "%P\n" | while read -r path; do
+        if [[ -L $path ]]; then
+            readlink "$path" | sha256sum | awk "{ print \$1 \"  \" $path}"
+        else
+            sha256sum "$path"
+        fi
+    done
+}
+
+main "$@"
