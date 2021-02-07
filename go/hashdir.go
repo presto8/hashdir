13990b6ff565be2a1f24552013c9052dc8b6231a
Preston Hunt <me@prestonhunt.com>
2021-02-07 08:40:50 -0800

    go: walk_path

--- go/hashdir.go
+++ go/hashdir.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "os"
+import "path/filepath"
+import "log"
+
+func walk_path(toppath string) {
+	err := filepath.Walk(toppath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, info.Size())
+			return nil
+		})
+
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	walk_path("../test_dir")
 }
