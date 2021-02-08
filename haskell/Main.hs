c3b09bc56fce1f3fc1c025638f30fd24189c7a9b
Preston Hunt <me@prestonhunt.com>
2021-02-07 21:03:38 -0800

    cleanup imports

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -3,49 +3,35 @@
 module Main where
 
 import Control.Monad (forM_, when)
-import qualified Data.ByteString
-import qualified Crypto.Hash.SHA256 as SHA256
-
 import Crypto.Hash.SHA256 (hashlazy)
+import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import System.Process (system)
-import Text.Printf (printf)
-
-import System.Directory.PathWalk
-import System.Posix.Files
-
-import Data.ByteString.Builder
-
 import qualified Data.Text as T
-import Data.Text(pack, unpack, replace)
+import Text.Printf (printf)
+import System.Directory.PathWalk
+-- import System.Posix.Files
 
 print_sha = print digest
   where
     digest = SHA256.finalize ctx
-    ctx    = foldl SHA256.update ctx0 (map Data.ByteString.pack [ [1, 2, 3] ] )
+    ctx    = foldl SHA256.update ctx0 (map B.pack [ [1, 2, 3] ] )
     ctx0   = SHA256.init
 
 print_sha2 = print $ SHA256.hash "hello"
 
 -- https://stackoverflow.com/questions/9502777/sha1-encoding-in-haskell
-
 hashFile :: FilePath -> IO B.ByteString
 hashFile = fmap hashlazy . BL.readFile
 
 toHex :: B.ByteString -> String
 toHex bytes = B.unpack bytes >>= printf "%02x"
 
--- hashEntry :: FilePath -> String
--- hashEntry path = hashFile path >>= putStrLn . toHex
-
-
 -- https://hackage.haskell.org/package/pathwalk-0.3.1.2/docs/System-Directory-PathWalk.html
-
 my_walk path = pathWalk path $ \dir subdirs files -> do
   forM_ files $ \file -> do
     let fullpath = dir ++ "/" ++ file
-    let fullpath' = unpack $ replace (pack path) "" (pack fullpath)
+    let fullpath' = T.unpack $ T.replace (T.pack path) "" (T.pack fullpath)
     hashFile fullpath >>= putStr . take 8 . toHex
     putStrLn $ "  ." ++ fullpath'
     -- when (getSymbolicLinkStatus file . isSymbolicLink) $ do
